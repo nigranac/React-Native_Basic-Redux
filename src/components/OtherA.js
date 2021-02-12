45e8ccf541c27815b6b2bd04a26b9122332b8c48
nigranac <can.argin.16@gmail.com>
2021-02-12 22:55:54 +0300

    bugFix

--- src/components/OtherA.js
+++ src/components/OtherA.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {useSelector, useDispatch} from 'react-redux';
+import {useSelector} from 'react-redux';
 const OtherA = () => {
   const myCounter = useSelector((can) => can.counter);
   return (
